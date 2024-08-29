5c8182c997416de3bde73e02179c5c08833c87aa
CodeWithMayuri <majurigashanmugananthan@gmail.com>
2024-08-29 17:12:14 +0530

    in middle of the project

--- app/src/main/java/com/example/storyrealm/services/AnalyticsService.java
+++ app/src/main/java/com/example/storyrealm/services/AnalyticsService.java
@@ -24,4 +24,18 @@ public class AnalyticsService {
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, title);
         firebaseAnalytics.logEvent("story_created", bundle);
     }
+
+    public void logStoryViewEvent(String storyId, String title) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, storyId);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, title);
+        firebaseAnalytics.logEvent("story_view", bundle);
+    }
+
+    public void logStoryLikeEvent(String storyId, String title) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, storyId);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, title);
+        firebaseAnalytics.logEvent("story_like", bundle);
+    }
 }
